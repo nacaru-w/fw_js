fe803111cee6394c043bbea6b8f31c8cfec267f3
Ignacio Casares Ruiz <icasaresr@uoc.edu>
2023-03-17 11:21:54 +0100

    Finished writing scripts

---
+++ PEC1_Ej2_2/script.js
@@ -0,0 +1,34 @@
+const currencyEl_one = document.getElementById("currency-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_one = document.getElementById("amount-one");
+const amountEl_two = document.getElementById("amount-two");
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+// Fetch exchange rates and update the DOM
+function calculate() {
+    let currency_one = currencyEl_one.value;
+    let currency_two = currencyEl_two.value;
+
+    fetch(`https://v6.exchangerate-api.com/v6/370d55c233982cc0f62a011c/latest/${currency_one}`)
+        .then(res => res.json())
+        .then(data => {
+            let rate = data.conversion_rates[currency_two]
+            rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2)
+        })
+}
+
+// Event listeners
+
+currencyEl_one.addEventListener("change", calculate);
+amountEl_one.addEventListener("input", calculate)
+currencyEl_two.addEventListener("change", calculate)
+amountEl_two.addEventListener("input", calculate)
+
+swap.addEventListener("click", () => {
+    let storage = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value
+    currencyEl_two.value = storage;
+    calculate()
+})
